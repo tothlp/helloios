36fbd96e62c435bd41517ce8d4638cd46503e09e
Tóth László <dev@tothlp.hu>
2024-03-07 08:08:25 +0100

    mod text

--- helloios/ContentView.swift
+++ helloios/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "swift")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, Swift!")
         }
         .padding()
     }
