9e226ba230e45b5d5b3bd9928b492dbf11969e7c
Tóth László <dev@tothlp.hu>
2024-03-07 08:05:16 +0100

    Initial Commit

---
+++ helloios/helloiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloiosApp.swift
+//  helloios
+//
+//  Created by Tóth László on 07/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct helloiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
